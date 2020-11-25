d885c2a0433f1ec7608bf96414656002c7e68e0c
palanikumar123456 <39948117+palanikumar123456@users.noreply.github.com>
2020-11-25 20:35:46 +0530

    Update day01_exercise_solution.py
    your review comments updated

--- day01_exercise_solution.py
+++ day01_exercise_solution.py
@@ -1,5 +1,5 @@
 print("***** 100DaysOfPython - Dr Angela Yu *****")
-print("Hello world")
+
 print("""Day01 Project Exercise: 
 \tBand Name Generator
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -13,3 +13,5 @@ print("Welcome to the Band Name Generator")
 city_name = input("What's the name of the city you grew up.? ")
 pets_name = input("What's your pet's name.? ")
 print("Your Band Name could be: " + city_name + " " + pets_name)
+
+print("Hello world")
