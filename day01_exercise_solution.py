78e7dea4612b28df8009d5fa69cab1696739b4ff
Your Name <you@example.com>
2020-11-25 14:18:45 +0530

    Day01 Band Name Generator Solution added

---
+++ day01_exercise_solution.py
@@ -0,0 +1,14 @@
+print("***** 100DaysOfPython - Dr Angela Yu *****")
+print("""Day01 Project Exercise: 
+\tBand Name Generator
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Note: Concatenation of Two Strings
+Which are of 'City Name' -and- Your 'Pet Name' details
+Q1: What is the CITY NAME where you grew up in.?
+Q2: What is your PET's NAME.?
+\n""")
+print("Day01 Project: 'Band Name Generation' Solution\n")
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up.? ")
+pets_name = input("What's your pet's name.? ")
+print("Your Band Name could be: " + city_name + " " + pets_name)
